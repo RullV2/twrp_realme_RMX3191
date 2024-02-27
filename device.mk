f0a60a52f501ec7b836a0308efef43e3680f0f92
RullV2 <136607912+RullV2@users.noreply.github.com>
2024-02-27 07:48:55 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -3,5 +3,14 @@ LOCAL_PATH := device/realme/RMX3191
 # Dynamic partition stuff
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Api
-PRODUCT_SHIPPING_API_LEVEL := 30
+# Fastboot packages
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery
+
+# Additional binaries & libraries needed for recovery
+TARGET_RECOVERY_DEVICE_MODULES += \
+	android.hardware.boot@1.2
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+	$(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.boot@1.2.so
